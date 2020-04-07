13343f0b8ef1707abeb5a329c3b756589a81c1ef
mahdihma <mahdi.haji@yahoo.com>
2020-04-07 14:08:30 +0430

    WeatherRequestHandler

--- app/src/main/java/com/example/hw1_mas/MainActivity.java
+++ app/src/main/java/com/example/hw1_mas/MainActivity.java
@@ -1,35 +1,30 @@
 package com.example.hw1_mas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.hw1_mas.models.City;
 import com.example.hw1_mas.utilities.NetWorkUtil;
-import com.example.hw1_mas.utilities.NetWorkUtil.*;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-//import com.mapbox.api.geocoding.v5.MapboxGeocoding;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URL;
-import java.text.BreakIterator;
-import java.util.ArrayList;
+
+//import com.mapbox.api.geocoding.v5.MapboxGeocoding;
 
 public class MainActivity extends AppCompatActivity {
     EditText locationSearchBox;
