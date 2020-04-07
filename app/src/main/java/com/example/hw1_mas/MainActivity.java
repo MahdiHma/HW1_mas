546f28aa5103c568424d51dfe9c23fe54c68c0d2
sreyhani <sajjad7997@gmail.com>
2020-04-07 20:40:54 +0430

    search result handler

--- app/src/main/java/com/example/hw1_mas/MainActivity.java
+++ app/src/main/java/com/example/hw1_mas/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.hw1_mas;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
@@ -32,8 +38,9 @@ public class MainActivity extends AppCompatActivity {
     EditText locationSearchBox;
     TextView searchResultTv;
     Button searchBtn;
-
-
+    LinearLayout llResults;
+    Handler mHandler;
+    private static final int SHOW_CITIES = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         locationSearchBox = findViewById(R.id.et_location_search);
         searchResultTv = findViewById(R.id.tv_search_result);
         searchBtn = findViewById(R.id.btn_search);
+        llResults = findViewById(R.id.ll_results);
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 sendSearchRequest(url);
             }
         });
+
 
     }
 
@@ -64,17 +73,49 @@ public class MainActivity extends AppCompatActivity {
                         Gson gson = new Gson();
                         JsonObject gResponse = gson.fromJson(response.toString(), JsonObject.class);
                         features = (JsonArray) gResponse.get("features");
-                        City city = gson.fromJson(features.get(0), City.class);
-                        searchResultTv.setText("Response: " + city.toString());
+                        ArrayList<City> resultCities = new ArrayList<>();
+                        for (JsonElement feature : features) {
+                            resultCities.add(gson.fromJson(feature,City.class));
+                        }
+                        Message message = new Message();
+                        message.what = SHOW_CITIES;
+                        message.obj = resultCities;
+                        mHandler.sendMessage(message);
                     }
                 }, new Response.ErrorListener() {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // TODO: Handle error
-
                     }
                 });
         queue.add(jsonObjectRequest);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what){
+                    case SHOW_CITIES:
+                        showCities((ArrayList<City>) msg.obj);
+                        break;
+                }
+            }
+        };
+    }
+
+    private void showCities(ArrayList<City> cities) {
+        for (City city : cities) {
+            Button btn = new Button(this);
+            btn.setText(city.getPlace_name());
+            llResults.addView(btn);
+        }
+
+        ;
     }
 }
