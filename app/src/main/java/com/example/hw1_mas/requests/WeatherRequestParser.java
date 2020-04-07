a87bbd298011986fdfa6abffd331727ad0e4b488
mahdihma <mahdi.haji@yahoo.com>
2020-04-07 18:09:47 +0430

    JsonParser and saver completed

--- app/src/main/java/com/example/hw1_mas/requests/WeatherRequestParser.java
+++ app/src/main/java/com/example/hw1_mas/requests/WeatherRequestParser.java
@@ -1,22 +1,49 @@
 package com.example.hw1_mas.requests;
 
 import com.example.hw1_mas.models.City;
+import com.example.hw1_mas.models.Day;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import org.json.JSONObject;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class WeatherRequestParser {
     private static WeatherRequestParser requestParser;
+    private static String fileLocation = "";
 
-    private WeatherRequestParser(){}
-
-    public static String saveJson() {
-        Gson fromJson = new Gson();
+    private WeatherRequestParser() {
     }
 
-    public static String fromJson(JsonObject weatherResponse) {
-        Gson fromJson = new Gson();
+    public static void saveJson(ArrayList<Day> day) {
+        Gson toJson = new GsonBuilder().setPrettyPrinting().create();
+        String dayToJsonString = toJson.toJson(day);
+        try {
+            FileWriter fileWriter = new FileWriter(WeatherRequestParser.fileLocation);
+            fileWriter.write(dayToJsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static ArrayList<Day> fromJson(JSONObject weatherResponse) {
+        Gson jsonReader = new Gson();
+        JsonObject weatherObject = jsonReader.fromJson(weatherResponse.toString(), JsonObject.class);
+        Day current = jsonReader.fromJson(weatherObject.get("current"), Day.class);
+        ArrayList<Day> days = new ArrayList<>();
+        days.add(current);
+        JsonArray forecasts = (JsonArray) weatherObject.get("forecast");
+        for (JsonElement day : forecasts) {
+            days.add(jsonReader.fromJson(day, Day.class));
+        }
+        return days;
 //        fromJson.fromJson(weatherResponse, );
 
     }
